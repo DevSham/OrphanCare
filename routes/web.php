f43967b21198aaa092aa2e15c5504992e9bb0182
DevSham <kjingo12@gmail.com>
2025-10-16 19:58:33 +0300

    add routes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('pages.skc-landing');
+})->name('skc.landing');
 
-
-})->name('skc.landing');;
+Route::get('/support', function () {
+    return view('pages.self-landing-page');
+})->name('skc.support');
